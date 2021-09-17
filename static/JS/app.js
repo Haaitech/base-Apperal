93a767b53ccfea7471da9a9da542c7514c47f2e8
Tom Haaima <tomhaaima@gmail.com>
2021-09-17 12:14:56 +0200

    first commit

---
+++ static/JS/app.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('input[id="email"]');
+const errorImg = document.querySelector(".error-img");
+const msg = document.querySelector(".validation-msg");
+
+input.addEventListener("invalid", function (e) {
+	if (e.target.validity.valueMissing) {
+		e.preventDefault();
+		errorImg.classList.add("error-msg");
+		msg.innerHTML = `please add your email!`;
+		setTimeout(function () {
+			errorImg.classList.remove("error-msg");
+			msg.innerHTML = ``;
+		}, 5000);
+	}
+	if (e.target.validity.typeMismatch) {
+		e.preventDefault();
+		errorImg.classList.add("error-msg");
+		msg.innerHTML = `Enter a valid email adress!`;
+		setTimeout(function () {
+			errorImg.classList.remove("error-msg");
+			msg.innerHTML = ``;
+		}, 5000);
+	}
+});
